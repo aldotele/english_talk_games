0bcee808187da9fa8a8840f495fb07ae9c85f545
aldotele <aldo.telese@hotmail.it>
2021-05-02 18:59:18 +0200

    separator optimized

--- guess_the_company.py
+++ guess_the_company.py
@@ -6,16 +6,16 @@ players = int(input("How many players? "))
 players_list = []
 
 for p in range(players):
-    player_name = input("What's your name? ")
+    player_name = input("Player's name: ")
     players_list.append(player_name)
-
+print('\nstarting ...')
 
 inf = open(os.path.join('files', 'sample.txt'))
 
 d = dict()
 
 for line in inf:
-    line = line.strip().split(" | ") # 0 -> letter, 1 -> suggestion , 2 -> answer
+    line = line.strip().split("|") # 0 -> letter, 1 -> suggestion , 2 -> answer
     d[line[0]] = {
         "suggestion": line[1],
         "answer": line[2]
@@ -25,19 +25,20 @@ for line in inf:
 
 j = 0
 for i in range(len(d)):
+    print()
     player = players_list[j]
     letter = input('{}, please choose a letter: '.format(player))
     while True:
         if len(letter) == 1 and ('A' <= letter <= 'Z' or 'a' <= letter <= 'z'):
             break
-        letter = input('not valid. Please write a letter: ')
+        letter = input('not valid. Please rewrite: ')
 
     letter = letter.upper()
     j += 1
     if j == players:
         j = 0
 
-    print('{}'.format(d[letter]['suggestion']))
+    print('{} - {}'.format(letter, d[letter]['suggestion']))
     guess = input('enter your guess: ')
     guess = guess.upper()
     actual_choice = d[letter]['answer']
