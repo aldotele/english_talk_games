9199fa795f48b4ecc68354a2c7d0952ccfb055e4
DazFather <39210887+DazFather@users.noreply.github.com>
2021-05-10 23:59:52 +0200

    General improving
    - Added comments
    - When player is choosing a letter it will be displayed all the avaiables
    - Restructuring the rounds mechanics
    - Display a leaderboard when game end and others changes on screen
    - general improving and optimization

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,91 +1,93 @@
 import os
 import base64
- 
 
-input_file = input('write the input file to use: ')
+# Input the file to use
 while True:
+    input_file = input('write the input file to use: ')
     try:
         inf = open(os.path.join('files', input_file))
         break
     except FileNotFoundError:
-        input_file = input('file not found, please rewrite: ')
+        print('[x]Error: file not found, please re', end='')
 
+# Input the number of the players
+players = -1
+for attempt in range(1, 4):
+    try:
+        players = int(input("How many players? "))
+        break
+    except:
+        print('[x]Error: invalid input, please retry (attempts: %d/3)' % attempt)
+if players == -1:
+    print('[x]ERROR: Too many attempts')
+    quit()
 
-players = int(input("How many players? "))
+# Initializing players scores and list.
 players_list = []
 scores = dict()
 
+# Take in input the players name and make it unique
 for p in range(players):
     player_name = input("Player's name: ")
     unique = f"{player_name}_{p}"
     players_list.append(unique)
     scores[unique] = 0
 
-print('\nstarting ...')
-
-
+# Loading all the questions into a dictionary
+print('\n[...] Starting the game')
 d = dict()
-
 for line in inf:
     line = line.strip().split("|")  # 0 -> letter, 1 -> suggestion , 2 -> answer
     d[line[0]] = {
         "suggestion": line[1],
         "answer": line[2]
     }
-    
-#print(d)
 
-j = 0
-for i in range(len(d)):
-    print()
-    player = players_list[j]
-    letter = input('{}, please choose a letter: '.format(player)).upper()
-    while True:
-        if len(letter) == 1 and ('A' <= letter <= 'Z' or 'a' <= letter <= 'z'):
-            try:
-                description = d[letter]['suggestion']       
-                break
-            except KeyError:
-                pass
+# Dividing the game into rounds
+rounds = len(d) // players
+for i in range(rounds):
+    print('\n[!] Round %d/%d' % (i + 1, rounds))
+    for player in players_list:
 
-        letter = input('not valid. Please rewrite: ').upper()
+        # Make player choose the letter
+        print('\tAvailable letters:', *[ letters for letters in d ])
+        while True:
+            letter = input('{}, please choose one: '.format(player)).upper()
+            if len(letter) == 1 and 'A' <= letter <= 'Z':
+                try:
+                    description = d[letter]['suggestion']
+                    break
+                except KeyError:
+                    pass
+            print('[x]Error: invalid input. Please retry')
 
-    j += 1
-    if j == players:
-        j = 0
+        # Input the guess of the user
+        print('{} - {}'.format(letter, description))
+        guess = input('enter your guess: ').strip().upper()
 
-    print('{} - {}'.format(letter, description))
+        # Decoding the real answer check if the guess is correct
+        actual_choice = d[letter]['answer']
+        base64_bytes = actual_choice.encode('ascii')
+        company_bytes = base64.b64decode(base64_bytes)
+        actual_choice = company_bytes.decode('ascii')
+        if guess == actual_choice:
+            scores[player] += 1
+            print('[v] CORRECT!\n')
+        else:
+            print('wrong... it was {}\n'.format(company))
+        del d[letter]
 
-    guess = input('enter your guess: ')
-    guess = guess.upper().strip()
-    actual_choice = d[letter]['answer']
-
-    base64_bytes = actual_choice.encode('ascii')
-    company_bytes = base64.b64decode(base64_bytes)
-    company = company_bytes.decode('ascii')
-
-    if guess == company:
-        scores[player] += 1
-        print('CORRECT!')
-    else:
-        print('wrong... it was {}'.format(company))
-
+# Finding the winner/s
 sorted_players = sorted(scores, key=scores.get, reverse=True)
 winner_points = scores[sorted_players[0]] # return the winner player points
-
 def find_winner(x):
-    if scores[x] == winner_points:
-        return True
-    else:
-        return False 
-    
+    return scores[x] == winner_points
 winners = list(filter(find_winner, sorted_players))
 
-print()
+# Announcing the winner/s and display the complete leaderboard
 if len(winners) == 1:
-    print('the winner is {}'.format(winners[0]))
+    print('\n\tThe winner is {}'.format(winners[0]))
 else:
-    print('winners are {}'.format(', '.join(winners)))
-
-
-    
+    print('\n\tThe winners are {}'.format(', '.join(winners)))
+for player in scores:
+    print(player, '\t-', scores[player])
